44ac10a94abfd0e9e2a0f3d5a95d3737c4d38fe3
Brian McPhillips <B.McPhillips89@gmail.com>
2021-10-13 18:08:47 -0700

    Article List component

--- src/components/Article/Article.test.jsx
+++ src/components/Article/Article.test.jsx
@@ -4,16 +4,15 @@ import Article from './Article';
 
 describe('Article component', () => {
   afterEach(() => cleanup());
-  
+
   it('renders a single article', () => {
     const { asFragment } = render(<Article 
-      urltoImage="http://placeholder.com/villager"
+      urlToImage="http://placeholder.com/villager"
       title="Best article ever"
       description="This is beyond a great article so insightful"
       author="test testerson"
-      url="www.test.test.com"  
+      url="www.test.com"  
     />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
-
