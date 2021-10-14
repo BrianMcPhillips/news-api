360e121b21d18c4f7b50fa9dc3f660d4cd7a2a25
Brian McPhillips <B.McPhillips89@gmail.com>
2021-10-13 20:42:36 -0700

    Items rendered to page

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import ArticleList from '../components/ArticleList/ArticleList';
+
 
 export default class NewsSearch extends Component {
+  state = {
+    articles: []
+  }
   
-  componentDidMount = () => {
+  componentDidMount = async() => {
     const input = 'bitcoin';
     fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=${process.env.REACT_APP_NEWS}`)
       .then(res => res.json())
-      .then(json => console.log(json));
+      .then(json => json.articles.map(article => ({
+        urlToImage: article.urlToImage,
+        title: article.title,
+        description: article.description,
+        author: article.author,
+        url: article.url
+      })))
+      .then(articleList => this.setState({ articles: articleList }));
   }
   render() {
+    const { articles } = this.state;
     return (
       <div>
-        Hello World
+        <ArticleList articles={articles} />
       </div>
     );
   }
