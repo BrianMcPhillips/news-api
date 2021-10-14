0ae96282d8a6cf22f5a5d636bc2317bc882b4aca
Brian McPhillips <B.McPhillips89@gmail.com>
2021-10-13 22:58:26 -0700

    Search feature working

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ArticleList from '../components/ArticleList/ArticleList';
+import Search from '../components/Search/Search';
 import { getArticles } from '../services/news-api';
+
 
 
 export default class NewsSearch extends Component {
   state = {
-    articles: []
+    articles: [],
+    searchTerm: ''
   }
   
   componentDidMount = async() => {
@@ -14,10 +17,26 @@ export default class NewsSearch extends Component {
       .then(articleList => this.setState({ articles: articleList }));
   }
 
+  handleChange = ({ target }) => {
+    this.setState({ searchTerm: target.value });
+  } 
+
+  handleSubmit = e => {
+    e.preventDefault();
+    getArticles(this.state.searchTerm)
+      .then(articleList => this.setState({ articles: articleList }));
+    
+  }
+
   render() {
-    const { articles } = this.state;
+    const { articles, searchTerm } = this.state;
     return (
       <div data-testid="allArticles">
+        <Search 
+          searchTerm={searchTerm}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+        /> 
         <ArticleList articles={articles} />
       </div>
     );
